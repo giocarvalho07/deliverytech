9872eff7dd077bd3e04375a21c027b6e1a142d96
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2026-02-07 16:59:01 -0300

    versão do projeto parte 22 - ajuste DTOS validacõe e schemas

--- delivery-api/src/main/java/com/deliverytech/delivery_api/dto/request/login/RegisterRequestDTO.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/dto/request/login/RegisterRequestDTO.java
@@ -1,6 +1,7 @@
 package com.deliverytech.delivery_api.dto.request.login;
 
 import com.deliverytech.delivery_api.enums.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,15 +18,19 @@ import lombok.Setter;
 public class RegisterRequestDTO {
 
     @NotBlank(message = "O nome é obrigatório")
+    @Schema(example = "João Silva", description = "Nome completo do cliente")
     private String nome;
 
     @NotBlank(message = "O e-mail é obrigatório")
     @Email(message = "Formato de e-mail inválido")
+    @Schema(example = "joao.silva@email.com", description = "Email completo do cliente")
     private String email;
 
     @NotBlank(message = "A senha é obrigatória")
+    @Schema(example = "senha@123", description = "Senha completa do cliente")
     private String senha;
 
     @NotNull(message = "A Role é obrigatória")
+    @Schema(example = "ADMIN", description = "ROLE do cliente")
     private UserRole role;
 }
